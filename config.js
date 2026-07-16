fc8a05046dd5a3a371ab87c73b496e841abed7b4
drMurtadha <murtadha@utm.my>
2026-07-16 09:53:39 +0800

    connect Google Drive backend

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.MPE_CONFIG = {
   // Tampal URL Web App Google Apps Script yang berakhir dengan /exec.
-  GOOGLE_SCRIPT_URL: "",
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzGN842JD67hoDXScYyz5pm8tGgHYuBSflfanw8R8xAEOd7OVPdUwxMhLr5-Zc5CQg9/exec",
   APP_NAME: "MPE Hub"
 };
